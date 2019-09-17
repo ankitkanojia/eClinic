7e737772815ef519418b086916ef83bb9a400121
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-17 10:08:10 +0530

    Initial set up of project
    Initial check in of project for development of AyurvedOnCall project

---
+++ AyurvedOnCall/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AyurvedOnCall
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
