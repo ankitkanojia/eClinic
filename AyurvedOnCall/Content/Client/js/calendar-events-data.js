a6b3a6018dd425edd3e64b2d95867a37cbd23627
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-18 17:05:42 +0530

    Theme set up
    set up the front end and back end theme files.
    Added script, css, content and image files.

---
+++ AyurvedOnCall/Content/Client/js/calendar-events-data.js
@@ -0,0 +1,158 @@
+var calendarEvents= [
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-01-01'
+    },
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-01-03'
+    },
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-01-22',
+        end: '2016-01-23'
+    },
+    {
+        id: 999,
+        title: 'Repeating Event',
+        url: 'http://google.com/',
+        start: '2016-01-23T16:00:00'
+    },
+    {
+        id: 999,
+        title: 'Repeating Event',
+        url: 'http://google.com/',
+        start: '2016-01-23T16:00:00'
+    },
+    {
+        title: 'Conference',
+        start: '2016-01-11',
+        end: '2016-01-13'
+    },
+    {
+        title: 'Meeting',
+        start: '2016-01-12T10:30:00',
+        end: '2016-01-12T12:30:00'
+    },
+    {
+        title: 'Lunch',
+        start: '2016-01-12T12:00:00'
+    },
+    {
+        title: 'Meeting',
+        start: '2016-01-12T14:30:00'
+    },
+    {
+        title: 'Dinner',
+        start: '2016-01-12T20:00:00'
+    },
+    
+
+
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-02-02'
+    },
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-02-04'
+    },
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-02-22',
+        end: '2016-02-23'
+    },
+    {
+        id: 999,
+        title: 'Repeating Event',
+        url: 'http://google.com/',
+        start: '2016-02-23T16:00:00'
+    },
+    {
+        id: 999,
+        title: 'Repeating Event',
+        url: 'http://google.com/',
+        start: '2016-02-23T16:00:00'
+    },
+    {
+        title: 'Conference',
+        start: '2016-02-11',
+        end: '2016-02-13'
+    },
+    {
+        title: 'Meeting',
+        start: '2016-02-12T10:30:00',
+        end: '2016-02-12T12:30:00'
+    },
+    {
+        title: 'Lunch',
+        start: '2016-02-12T12:00:00'
+    },
+    {
+        title: 'Meeting',
+        start: '2016-02-12T14:30:00'
+    },
+    {
+        title: 'Dinner',
+        start: '2016-01-12T20:00:00'
+    },
+    
+
+
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-03-01'
+    },
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-03-02'
+    },
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-03-21',
+        end: '2016-03-22'
+    },
+    {
+        id: 999,
+        title: 'Repeating Event',
+        url: 'http://google.com/',
+        start: '2016-03-23T16:00:00'
+    },
+    {
+        id: 999,
+        title: 'Repeating Event',
+        url: 'http://google.com/',
+        start: '2016-03-23T16:00:00'
+    },
+    {
+        title: 'Conference',
+        start: '2016-03-11',
+        end: '2016-03-13'
+    },
+    {
+        title: 'Meeting',
+        start: '2016-03-12T10:30:00',
+        end: '2016-03-12T12:30:00'
+    },
+    {
+        title: 'Lunch',
+        start: '2016-03-12T12:00:00'
+    },
+    {
+        title: 'Meeting',
+        start: '2016-03-12T14:30:00'
+    },
+    {
+        title: 'Dinner',
+        start: '2016-03-12T20:00:00'
+    }
+];
