f2cbfe39d0800b633c3899649e926f399d71aaa7
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-19 10:30:58 +0530

    Helper files added
    CommonFunctions for dropdown
    CookieHelper
    Static Values
    Layout for set up master theme for client panel

---
+++ AyurvedOnCall/Helpers/StaticValues.cs
@@ -0,0 +1,10 @@
+﻿namespace AyurvedOnCall.Helpers
+{
+    public static class StaticValues
+    {
+        public static string SessionUserId => "UserId";
+        public static string SessionRoleId => "RoleId";
+        public static string SessionFullName => "FullName";
+        public static string SessionProfileImg => "ProfileImg";
+    }
+}
