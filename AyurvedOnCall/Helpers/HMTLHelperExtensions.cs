bad2a99569dc411597f99437812bd067843e6fbd
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-20 10:50:21 +0530

    Theme set up for admin panel

---
+++ AyurvedOnCall/Helpers/HMTLHelperExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace AyurvedOnCall.Helpers
+{
+    public static class HmtlHelperExtensions
+    {
+        public static string IsSelected(this HtmlHelper html, string controller = null, string action = null, string cssClass = null)
+        {
+
+            if (string.IsNullOrEmpty(cssClass))
+                cssClass = "active";
+
+            var currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            var currentController = (string)html.ViewContext.RouteData.Values["controller"];
+
+            if (string.IsNullOrEmpty(controller))
+                controller = currentController;
+
+            if (string.IsNullOrEmpty(action))
+                action = currentAction;
+
+            return controller == currentController && action == currentAction ?
+                cssClass : string.Empty;
+        }
+
+        public static string PageClass(this HtmlHelper html)
+        {
+            var currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            return currentAction;
+        }
+
+    }
+}
