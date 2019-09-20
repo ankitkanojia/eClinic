e3cfd4a0def421b7071db62a6f437265158efb06
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-20 09:25:59 +0530

    Configuration Files added
    Enumlist file added
    AdminController added and relaetd action files added
    Package install sendgrid and sendmail using sendgrid is done

---
+++ AyurvedOnCall/Helpers/EnumList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AyurvedOnCall.Helpers
+{
+    public class EnumList
+    {
+        public enum Roles
+        {
+            Admin = 1,
+            Doctor = 2,
+            Patient = 3
+        }
+
+        public enum EmailTemplete
+        {
+            ForgotPassword = 1,
+            DoctorRegistration = 2,
+            AppointmentAccept = 3,
+            AppointmentReject = 4
+        }
+    }
+}
---
+++ AyurvedOnCall/Helpers/SendgridEmailHelpers.cs
@@ -0,0 +1,68 @@
+﻿using AyurvedOnCall.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace AyurvedOnCall.Helpers
+{
+    public static class SendgridEmailHelpers
+    {
+        public static bool SendTempleteEmail(long templeteId, string to, string username, Dictionary<string, string> replacements)
+        {
+            var response = false;
+            try
+            {
+                using (var db = new DBEntities())
+                {
+                    var templete = db.EmailTempletes.FirstOrDefault(s => s.EmailTempleteId == templeteId);
+
+                    if (templete != null)
+                    {
+                        var body = replacements.Aggregate(templete.Body, (result, s) => result.Replace(s.Key, s.Value));
+
+                        var apiKey = ConfigurationManager.AppSettings["SendGrid"];
+                        var client = new SendGridClient(apiKey);
+                        var from = new EmailAddress("noreply@ayurvedoncall.com", ConfigurationManager.AppSettings["DefaultEmailUser"]);
+                        var toAddress = new EmailAddress(to, username);
+                        var plainTextContent = string.Empty;
+                        var htmlContent = body;
+                        var msg = MailHelper.CreateSingleEmail(from, toAddress, templete.Subject, plainTextContent, htmlContent);
+                        var sendEmailAsync = client.SendEmailAsync(msg);
+                        response = true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                response = false;
+            }
+            return response;
+        }
+
+        public static bool SendEmail(string subject, string body, string to, string username)
+        {
+            bool response;
+            try
+            {
+                var apiKey = ConfigurationManager.AppSettings["SendGrid"]; //Set you're sendgri key in websconfig
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress(ConfigurationManager.AppSettings["DefaultFromEmail"], ConfigurationManager.AppSettings["DefaultEmailUser"]);
+                var toAddress = new EmailAddress(to, username);
+                var plainTextContent = string.Empty;
+                var htmlContent = body;
+                var msg = MailHelper.CreateSingleEmail(from, toAddress, subject, plainTextContent, htmlContent);
+                var sendEmailAsync = client.SendEmailAsync(msg);
+                response = true;
+            }
+            catch (Exception)
+            {
+                response = false;
+            }
+            return response;
+        }
+    }
+}
