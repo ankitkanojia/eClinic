b2c9e9ba39d3c480345beb5c804b3e72a167555a
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-23 11:12:50 +0530

    Added authorization helper class and set up Doctor admin module
    Create admin doctor module in admin portal and related views are added .
    Create helper class of authorization in helper folder which verify or authorized the user.

---
+++ AyurvedOnCall/Models/ViewModels/ADoctorVm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AyurvedOnCall.Models.ViewModels
+{
+    public class DoctorVm
+    {
+        public long DoctorId { get; set; }
+        public string Speciality { get; set; }
+        public string FullName { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public DateTime DoctoringStartDate { get; set; }
+        public string ProfileImg { get; set; }
+        public string GenderId { get; set; }
+        public string Degree { get; set; }
+        public string University { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
