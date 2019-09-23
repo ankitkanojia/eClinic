a2519f36095bc4b8f310c548658482bef86d6712
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-23 17:43:58 +0530

    Inquiry module integration
    Inquiry module integration create controller and view file

---
+++ AyurvedOnCall/Controllers/AEnquiryController.cs
@@ -0,0 +1,32 @@
+﻿using AyurvedOnCall.Helpers;
+using AyurvedOnCall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AyurvedOnCall.Controllers
+{
+    [CheckAdminAuthorization]
+    public class AEnquiryController : Controller
+    {
+        private readonly DBEntities _dbEntities = new DBEntities();
+        public ActionResult Index()
+        {
+            try
+            {
+                using (_dbEntities)
+                {
+                    var data = _dbEntities.Enquiries.ToList();
+                    return View(data);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
