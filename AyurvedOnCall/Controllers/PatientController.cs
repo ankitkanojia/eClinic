77e94db5b94dae1fe611a31f3803830172e029e9
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-27 15:23:28 +0530

    Added appointment functionality
    appontment page added

--- AyurvedOnCall/Controllers/PatientController.cs
+++ AyurvedOnCall/Controllers/PatientController.cs
@@ -30,5 +30,45 @@ namespace AyurvedOnCall.Controllers
             RegenerateTempData();
             return View();
         }
+
+        public ActionResult Appointment(long id = 0)
+        {
+            try
+            {
+                using (_dbEntities)
+                {
+                    RegenerateTempData();
+                    var model = new Appointment();
+
+                    var doctor = _dbEntities.Doctors.Find(id);
+                    if (doctor != null)
+                    {
+                        model.DoctorId = doctor.DoctorId;
+                    }
+
+                    var userId = CookieHelper.Get(StaticValues.SessionUserId);
+
+                    if (!string.IsNullOrWhiteSpace(userId))
+                    {
+                        var uid = Convert.ToInt64(userId);
+                        var user = _dbEntities.UserMasters.Find(uid);
+                        if (user != null)
+                        {
+                            model.Email = user.Email;
+                            model.Mobile = user.Mobile;
+                            model.Fullname = user.FullName;
+                        }
+                    }
+
+
+                    return View(model);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
     }
 }
