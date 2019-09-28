38bfaaec63ac5acb871995f4fdc676cbc077dd92
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-28 17:32:36 +0530

    Default page or index page added in doctor module

--- AyurvedOnCall/Controllers/DoctorController.cs
+++ AyurvedOnCall/Controllers/DoctorController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using AyurvedOnCall.Helpers;
+using AyurvedOnCall.Models;
+using AyurvedOnCall.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AyurvedOnCall.Controllers
@@ -9,8 +11,24 @@ namespace AyurvedOnCall.Controllers
     public class DoctorController : Controller
     {
         // GET: Doctor
+        private readonly DBEntities _dbEntities = new DBEntities();
+
+        public void RegenerateTempData()
+        {
+            if (TempData["Success"] != null)
+            {
+                TempData["Success"] = TempData["Success"];
+            }
+
+            if (TempData["Error"] != null)
+            {
+                TempData["Error"] = TempData["Error"];
+            }
+        }
+
         public ActionResult Index()
         {
+            RegenerateTempData();
             return View();
         }
     }
