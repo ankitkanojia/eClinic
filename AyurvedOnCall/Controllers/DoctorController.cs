1142abcd00f47b9bca0912229c51de6bd6c6d9b7
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-29 11:58:01 +0530

    Added index action to the doctor module

--- AyurvedOnCall/Controllers/DoctorController.cs
+++ AyurvedOnCall/Controllers/DoctorController.cs
@@ -1,16 +1,37 @@
-﻿using System;
+﻿using AyurvedOnCall.Helpers;
+using AyurvedOnCall.Models;
+using AyurvedOnCall.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Security.Principal;
 using System.Web.Mvc;
+using System.Web.Security;
+using Exception = System.Exception;
 
 namespace AyurvedOnCall.Controllers
 {
     public class DoctorController : Controller
     {
-        // GET: Doctor
+        private readonly DBEntities _dbEntities = new DBEntities();
+
+        public void RegenerateTempData()
+        {
+            if (TempData["Success"] != null)
+            {
+                TempData["Success"] = TempData["Success"];
+            }
+
+            if (TempData["Error"] != null)
+            {
+                TempData["Error"] = TempData["Error"];
+            }
+        }
+
+        [CheckDoctorAuthorization]
         public ActionResult Index()
         {
+            RegenerateTempData();
             return View();
         }
     }
