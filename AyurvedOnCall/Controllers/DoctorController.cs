aa45ec461ad69b6011b3f681dc2b83918b4f2d18
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-29 11:59:30 +0530

    Added GetDoctorCalender event to get appointment dates

--- AyurvedOnCall/Controllers/DoctorController.cs
+++ AyurvedOnCall/Controllers/DoctorController.cs
@@ -28,11 +28,78 @@ namespace AyurvedOnCall.Controllers
             }
         }
 
+        #region --> Dashboard
         [CheckDoctorAuthorization]
         public ActionResult Index()
         {
             RegenerateTempData();
             return View();
         }
+
+        public JsonResult GetDoctorCalender()
+        {
+            try
+            {
+                using (_dbEntities)
+                {
+                    var userid = CookieHelper.Get(StaticValues.SessionUserId);
+                    long doctorId = 0;
+                    if (!string.IsNullOrWhiteSpace(userid))
+                    {
+                        doctorId = Convert.ToInt64(userid);
+                    }
+                    var response = new List<CalenderVm>();
+                    var appointment = _dbEntities.Appointments.Where(s => s.DoctorId == doctorId).ToList();
+
+                    if (appointment.Any())
+                    {
+                        foreach (var item in appointment)
+                        {
+                            if (Request.Url != null)
+                            {
+                                var calData = new CalenderVm
+                                {
+                                    id = item.AppointmentId,
+                                    end = item.AppointmentDate.ToString("yyyy-MM-dd"),
+                                    start = item.AppointmentDate.ToString("yyyy-MM-dd")
+                                };
+
+                                if (Request.Url != null)
+                                {
+                                    calData.url = Url.Action("Appointments", "Doctor", "", protocol: Request.Url.Scheme);
+                                }
+
+                                if (item.IsRejected)
+                                {
+                                    calData.title = "Rejected Apmt: " + item.Fullname;
+                                }
+                                else
+                                {
+                                    if (item.IsConfirm)
+                                    {
+                                        calData.title = "Confirm Apmt: " + item.Fullname;
+                                    }
+                                    else
+                                    {
+                                        calData.title = "Pending Apmt: " + item.Fullname;
+                                    }
+                                }
+
+                                response.Add(calData);
+                            }
+                        }
+                    }
+
+
+                    return Json(new { status = true, data = response.ToArray() }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        #endregion
     }
 }
