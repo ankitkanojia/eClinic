6a8f666bca9554b38e1ee0b00a1d14c8403d624e
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-30 09:38:32 +0530

    Added available slab view modal

--- AyurvedOnCall/Controllers/PatientController.cs
+++ AyurvedOnCall/Controllers/PatientController.cs
@@ -69,6 +69,106 @@ namespace AyurvedOnCall.Controllers
                 Console.WriteLine(e);
                 throw;
             }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Appointment(Appointment data)
+        {
+            try
+            {
+                using (_dbEntities)
+                {
+                    data.CreatedDate = DateTime.Now;
+                    data.IsActive = true;
+                    data.IsDelete = false;
+                    data.IsRejected = false;
+                    data.IsConfirm = false;
+                    _dbEntities.Appointments.Add(data);
+                    _dbEntities.SaveChanges();
+
+                    TempData["Success"] = "Appointment request added successfully. Wait for confirmation from doctor.";
+                }
+
+                return RedirectToAction("Appointment");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        public JsonResult GetDoctorCalender()
+        {
+            try
+            {
+                using (_dbEntities)
+                {
+                    var response = new List<CalenderVm>();
+                    var userid = CookieHelper.Get(StaticValues.SessionUserId);
+
+                    if (!string.IsNullOrWhiteSpace(userid))
+                    {
+                        var uid = Convert.ToInt64(userid);
+                        var user = _dbEntities.UserMasters.Find(uid);
+
+                        if (user != null)
+                        {
+                            var appointment = _dbEntities.Appointments.Where(s => s.Email == user.Email).ToList();
+
+                            if (appointment.Any())
+                            {
+                                foreach (var item in appointment)
+                                {
+                                    if (Request.Url != null)
+                                    {
+                                        var calData = new CalenderVm
+                                        {
+                                            id = item.AppointmentId,
+                                            end = item.AppointmentDate.ToString("yyyy-MM-dd"),
+                                            start = item.AppointmentDate.ToString("yyyy-MM-dd")
+                                        };
+
+                                        //if (Request.Url != null)
+                                        //{
+                                        //    calData.url = Url.Action("Appointment", "Patient", "", protocol: Request.Url.Scheme);
+                                        //}
+
+                                        if (item.IsRejected)
+                                        {
+                                            calData.title = item.Doctor.FullName + " Rejected Appointment ";
+                                        }
+                                        else
+                                        {
+                                            if (item.IsConfirm)
+                                            {
+                                                calData.title = item.Doctor.FullName + " Confirm Appointment ";
+                                            }
+                                            else
+                                            {
+                                                calData.title = item.Doctor.FullName + " Pending Appointment  ";
+                                            }
+                                        }
+
+                                        response.Add(calData);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
+                    return Json(new { status = true, data = response.ToArray() }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
